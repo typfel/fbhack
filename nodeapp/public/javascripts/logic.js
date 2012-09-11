49dbc2768406c0b9219c54add182242c2f2d427f
Seib <seibpoirier@gmail.com>
2012-09-11 16:48:10 +0200

    added basic opengraph publicatio

--- nodeapp/public/javascripts/logic.js
+++ nodeapp/public/javascripts/logic.js
@@ -5,12 +5,16 @@ H.init = function() {
 };
 
 H.startGame = function() {
-    FB.api("movikwiz:start",
+    FB.api("/me/movikwiz:start",
         'post',
-        {game: "http://somewhere"},
+        {game: "http://samples.ogp.me/258720674248098"},
         function(response) {
             console.log(response);
         }
     );
+};
+
+H.publishScore = function(score) {
+
 };
 
