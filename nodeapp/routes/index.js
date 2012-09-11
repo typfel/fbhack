bab45d8ef2ead8c51405c7a1e920bbc7fb6fbb90
Jacob Persson <jacob.persson@gmail.com>
2012-09-11 16:05:02 +0200

    add question view

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', 
+  	questions: [
+		{ 
+			description: 'Which actor was in *******',
+			option1: 'John Doe1',
+			option2: 'John Doe2',
+			option3: 'John Doe3',
+			option4: 'John Doe4',
+		}
+	]});
 };
